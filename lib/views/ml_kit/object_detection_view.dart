c381f1e2885eb9385fe35b892f74ecd2362bcf63
fehmi <fehmi_ali@hotmail.com>
2024-01-05 14:37:39 +0300

    object detection - image labeling with file picker

--- lib/views/ml_kit/object_detection_view.dart
+++ lib/views/ml_kit/object_detection_view.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+import 'package:google_mlkit_image_labeling/google_mlkit_image_labeling.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mlkit_object_detection/google_mlkit_object_detection.dart';
 
@@ -9,47 +12,68 @@ class ObjectDetectionView extends StatefulWidget {
 }
 
 class _ObjectDetectionViewState extends State<ObjectDetectionView> {
-  final InputImage inputImage = InputImage.fromFilePath(
-      'https://static.nadirkitap.com/fotograf/85580/33/Kitap_202307300032068558011.jpg');
-  // Use DetectionMode.stream when processing camera feed.
-// Use DetectionMode.single when processing a single image.
-
-// final objectDetector = ObjectDetector(options: options);
-
-  Future imgProcess() async {
-    final mode = DetectionMode.single;
-
-    final options = ObjectDetectorOptions(
-      mode: DetectionMode.single,
-      classifyObjects: true,
-      multipleObjects: true,
-    );
-    print(mode);
-    final objectDetector = ObjectDetector(options: options);
-    final List<DetectedObject> objects =
-        await objectDetector.processImage(inputImage);
-
-    // for (DetectedObject detectedObject in objects) {
-    //   print(detectedObject);
-    //   // final rect = detectedObject.boundingBox;
-    //   // final trackingId = detectedObject.trackingId;
-
-    //   // for (Label label in detectedObject.labels) {
-    //   //   print('${label.text} ${label.confidence}');
-    //   // }
-    // }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    imgProcess();
-  }
+  File? imageUrl;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-    );
+        appBar: AppBar(),
+        body: Column(
+          children: [
+            TextButton(
+                onPressed: () async {
+                  final image =
+                      await FilePicker.platform.pickFiles(type: FileType.image);
+
+                  final InputImage inputImage =
+                      InputImage.fromFile(File(image!.paths[0]!));
+                  // const mode = DetectionMode.single;
+
+                  final options = ObjectDetectorOptions(
+                    mode: DetectionMode.single,
+                    classifyObjects: true,
+                    multipleObjects: true,
+                  );
+                  final objectDetector = ObjectDetector(options: options);
+                  try {
+                    final List<DetectedObject> objects =
+                        await objectDetector.processImage(inputImage);
+                    print(objects);
+                    for (DetectedObject detectedObject in objects) {
+                      print(detectedObject.boundingBox);
+                      print(detectedObject.trackingId);
+                      for (Label label in detectedObject.labels) {
+                        print('${label.text} ${label.confidence}');
+                      }
+                    }
+                  } catch (e) {
+                    print(e);
+                  }
+                  objectDetector.close();
+                  setState(() {
+                    if (image.paths[0] != null) {
+                      imageUrl = File(image.paths[0]!);
+                    }
+                  });
+                  print('----------------------------------------');
+                  final ImageLabelerOptions options2 =
+                      ImageLabelerOptions(confidenceThreshold: 0.5);
+                  final imageLabeler = ImageLabeler(options: options2);
+                  final List<ImageLabel> labels =
+                      await imageLabeler.processImage(inputImage);
+
+                  for (ImageLabel label in labels) {
+                    final String text = label.label;
+                    final int index = label.index;
+                    final double confidence = label.confidence;
+                    print(text);
+                    print(index);
+                    print(confidence);
+                  }
+                },
+                child: const Text('Galeri')),
+            if (imageUrl != null) Image.file(imageUrl!),
+          ],
+        ));
   }
 }
